b1fdb1b6aa1de290977fadb757a208b148328209
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2024-12-29 16:54:14 -0300

    Joguinho do Mário finalizado

--- js/script.js
+++ js/script.js
@@ -1,29 +1,59 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+let isJumping = false;
+
+// Função de pulo
 const jump = () => {
-    if (!mario.classList.contains('jump')) { // Impede múltiplos saltos
-        mario.classList.add('jump');
+    if (!isJumping) { 
+        isJumping = true;
+        mario.classList.add('jump'); 
 
         setTimeout(() => {
-            mario.classList.remove('jump');
-        }, 500); // Tempo deve ser igual ao tempo da animação
+            mario.classList.remove('jump'); 
+            isJumping = false; 
+        }, 500); 
     }
-}
+};
 
-const loop = setInterval(() => {
+// Função de detecção de colisão
+const checkCollision = () => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const marioPosition = parseFloat(
+        window.getComputedStyle(mario).bottom.replace('px', '')
+    ); 
 
-    // Verifica a colisão
+    // Verifica colisão
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`; 
-        mario.style.animation = 'none'; 
-        mario.style.bottom = `${marioPosition}px`; 
-        alert('Game Over!'); 
-        clearInterval(loop);   
-    }
-}, 10);
+        pipe.style.left = `${pipePosition}px`;
 
-document.addEventListener('keydown', jump);
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '/assets/images/game-over.png'; 
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+};
+
+// Função de animação de colisão
+const gameLoop = () => {
+    checkCollision();
+    requestAnimationFrame(gameLoop); 
+};
+
+// Inicia o loop de animação
+requestAnimationFrame(gameLoop);
+
+
+document.addEventListener('keydown', (event) => {
+    const validKeys = ['Space', 'ArrowUp']; 
+    if (validKeys.includes(event.code)) {
+        if (!isJumping) { 
+            jump();
+        }
+    }
+});
